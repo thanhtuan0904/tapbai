901fe9bffd287587446b46d9a14ba8f84d2f603d
thanhtuan0904 <iamtuan090404@gmail.com>
2025-03-26 19:09:23 +0700

    up

---
+++ bai3/bai3/Program.cs
@@ -0,0 +1,49 @@
+﻿public class hinhChuNhat
+{
+    double chieudai;
+    double chieurong;
+    public hinhChuNhat()
+    {
+
+    }
+    public hinhChuNhat(double chieudai , double chieurong)
+    {
+        this.chieurong = chieurong;
+        this.chieudai = chieudai;
+
+    }
+
+    public double chuViHCN(double chieudai , double chieurong)
+    {
+        return (chieudai + chieurong) * 2;
+    }
+    public double dienTichHCN(double chieudai, double chieurong)
+    {
+        return chieudai * chieurong;
+    }
+    public static void Main(string[] args)
+    {
+       Console.WriteLine("nhap chieu dai");
+        double chieudai = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("nhap chieu rong:");
+        double chieurong = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("nhap 1 tinh chu vi , nhap 2 tinh dien tich");
+        int n = Convert.ToInt32(Console.ReadLine());
+        hinhChuNhat hcn = new hinhChuNhat(chieudai, chieurong);
+        if (n == 1)
+        {
+            double chuvi = hcn.chuViHCN(chieudai, chieurong);
+            Console.WriteLine("chu vi hcn:"+chuvi);
+        }
+        else if (n == 2)
+        {
+            double dientich = hcn.dienTichHCN(chieudai, chieurong);
+            Console.WriteLine("dien tich hcn:"+dientich);
+        }
+        else
+        {
+            Console.WriteLine("loi");
+        }
+    }
+} 
