fde3477619a4ef665573af90585ab5255660581d
thanhtuan0904 <iamtuan090404@gmail.com>
2025-03-26 18:11:45 +0700

    them

---
+++ bai2/bai2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 3, 5, 1, 7, 2 };
+        int minIndex = MinValue(arr); // Gọi hàm MinValue
+        Console.WriteLine("Chỉ số của giá trị nhỏ nhất là: " + minIndex);
+        Console.WriteLine("Giá trị nhỏ nhất là: " + arr[minIndex]);
+    }
+
+    public static int MinValue(int[] arr)
+    {
+        if (arr == null || arr.Length == 0)
+        {
+            throw new ArgumentException("Mảng không được rỗng.");
+        }
+
+        int min = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+                index = i;
+            }
+        }
+
+        return index; // Trả về index của giá trị nhỏ nhất
+    }
+}
