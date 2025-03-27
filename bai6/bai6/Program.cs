5fe61d5d00c349c37e756f6d494774faa6d11273
thanhtuan0904 <iamtuan090404@gmail.com>
2025-03-28 00:27:35 +0700

    upload

---
+++ bai6/bai6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public abstract class Animal
+{
+    protected string tiengKeu { get; set; }
+    public Animal(string tiengKeu)
+    {
+        this.tiengKeu = tiengKeu;
+    }
+    public abstract void PrintInfo(); // pham vi truy cap la cong khai :
+
+
+}
+class Cat : Animal
+{
+    private string giongMeo { get; set; }
+    public Cat(string tiengKeu, string giongMeo) : base(tiengKeu)
+    {
+        this.giongMeo = giongMeo;
+    }
+    public override void PrintInfo()
+    {
+        Console.WriteLine($"Tiếng kêu: {tiengKeu}");
+        Console.WriteLine($"Giống mèo: {giongMeo}");
+    }
+    static void Main(string[] args)
+    {
+        Cat cat = new Cat("meo meo", "meo cong nghiep");
+        cat.PrintInfo();
+
+    }
+}
+
+
